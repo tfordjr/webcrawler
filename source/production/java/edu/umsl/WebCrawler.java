ba9289aa0f39115e54af4cb98f401beacec49d18
tfordjr <98005445+tfordjr@users.noreply.github.com>
2022-04-25 17:07:06 -0500

    Basics built, WebCrawler navigates web pages from multiple sites, attempting to limit it to wikipedia.

--- source/production/java/edu/umsl/WebCrawler.java
+++ source/production/java/edu/umsl/WebCrawler.java
@@ -1,19 +1,67 @@
 package edu.umsl;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import java.util.Scanner;
+import java.util.ArrayList;
 
-public class WebCrawler extends Application {
-    public void start(Stage primaryStage) throws Exception {
-        Button button = new Button("OK");
-        Scene scene = new Scene(button, 300, 250);
-        primaryStage.setTitle("WebCrawler");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+public class WebCrawler {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a URL: ");
+        String url = input.nextLine();
+        crawler(url); // Traverse the Web from the starting url
     }
 
-    public static void main(String[] args) {
-        Application.launch(args);
+    public static void crawler(String startingURL) throws InterruptedException {
+
+        ArrayList<String> listOfPendingURLs = new ArrayList<>();
+        ArrayList<String> listOfTraversedURLs = new ArrayList<>();
+        listOfPendingURLs.add(startingURL);
+
+        while (!listOfPendingURLs.isEmpty() &&
+                listOfTraversedURLs.size() <= 100) {
+            String urlString = listOfPendingURLs.remove(0);
+            if (!listOfTraversedURLs.contains(urlString)) {
+                listOfTraversedURLs.add(urlString);
+                System.out.println("Crawl " + urlString);
+
+                try {
+                    Thread.sleep(50);
+                } catch (Exception ex) {
+                    System.out.println("Error: " + ex.getMessage());
+                }
+
+                for (String s : getSubURLs(urlString)) {
+                    if (!listOfTraversedURLs.contains(s))
+                        listOfPendingURLs.add(s);
+                }
+            }
+        }
+    }
+
+    public static ArrayList<String> getSubURLs(String urlString) throws InterruptedException {
+        ArrayList<String> list = new ArrayList<>();
+
+        try {
+            java.net.URL url = new java.net.URL(urlString);
+            Scanner input = new Scanner(url.openStream());
+            int current = 0;
+            while (input.hasNext()) {
+                String line = input.nextLine();
+                current = line.indexOf("http:", current);
+                while (current > 0) {
+                    int endIndex = line.indexOf("\"", current);
+                    if (endIndex > 0) { // Ensure that a correct URL is found
+                        list.add(line.substring(current, endIndex));
+                        current = line.indexOf("http:", endIndex);
+                    } else
+                        current = -1;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+            if (Thread.interrupted())  // Clears interrupted status!
+                throw new InterruptedException();
+        }
+
+        return list;
     }
 }
